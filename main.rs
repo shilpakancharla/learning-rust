f603389598fe0e17be079c37ada8728f5f5bfb00
Shilpa Kancharla <shilpakancharla18@gmail.com>
2021-05-27 09:01:49 -0400

    average function

--- main.rs
+++ main.rs
@@ -1,4 +1,12 @@
-// demo
 fn main() {
-    println!("Hello, world!");
+    // Three variables with different data types
+    let a = 13;
+    let b = 2.3;
+    let c: f32 = 120.0;
+
+    // Cast a as a 64 bit float
+    let average = (a as f64 + b + c as f64) / 3.0;
+
+    assert_eq!(average, 45.1);
+    println!("Test passed.")
 }
