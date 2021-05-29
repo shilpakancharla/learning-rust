11d96d5245cb8873c300054f05d1621ce6db2b93
Shilpa Kancharla <shilpakancharla18@gmail.com>
2021-05-29 12:45:20 -0400

    higher lower

---
+++ higher_lower/src/main.rs
@@ -0,0 +1,24 @@
+use std::io;
+use random::prelude::*;
+
+fn main() {
+    let secret_number = random::thread_rng().gen_range(1..101);
+
+    println!("I'm thinking of a number between 1 and 100...");
+    println!("Guess the number: ");
+
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read input line.");
+        let guess: u32 = guess.trim().parse().expect("Failed to parse the guess.");
+
+        if guess > secret_number {
+            println!("\n{} is too high! Guess lower: ", guess);
+        } else if guess < secret_number {
+            println!("\n{} is too low! Guess higher: ", guess);
+        } else {
+            println!("\nYou got it! The secret number was {}.", secret_number);
+            break;
+        }
+    }
+}
---
+++ rand.rs
@@ -0,0 +1,6 @@
+use rand::random;
+
+fn main() {
+    let number = rand::random::<f64>();
+    println!("number is {}", number);
+}
